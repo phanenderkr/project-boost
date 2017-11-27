cd75dd428c66cf11c84b744af2bb48e5f82be24c
Phanender Reddy Kasireddy <phanender100@gmail.com>
2017-11-27 15:28:50 +0530

    Thrust Movement

---
+++ Assets/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour{
+	private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start (){
+		rigidBody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		ProcessInput();
+	}
+
+	void ProcessInput(){
+		//can thrust while rotating
+		if (Input.GetKey(KeyCode.Space)){
+			rigidBody.AddRelativeForce(Vector3.up * 1);
+			print("thrusting");
+		}
+
+		if (Input.GetKey(KeyCode.A)){
+			print("Roate left");
+		} else if (Input.GetKey(KeyCode.D)){
+			print("Roate left");
+		}
+	}
+}
