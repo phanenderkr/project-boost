e62fff7a31111829bfcb304399e01ab2cc555b1c
Phanender Reddy Kasireddy <phanender100@gmail.com>
2017-11-27 17:44:17 +0530

    Add directions and sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour{
 	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+	
 	// Use this for initialization
 	void Start (){
 		rigidBody = gameObject.GetComponent<Rigidbody>();
+		audioSource = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -18,12 +21,19 @@ public class Rocket : MonoBehaviour{
 		//can thrust while rotating
 		if (Input.GetKey(KeyCode.Space)){
 			rigidBody.AddRelativeForce(Vector3.up * 1);
-			print("thrusting");
+			if (!audioSource.isPlaying){
+				audioSource.Play();
+			}	
+		}
+		if (Input.GetKeyUp(KeyCode.Space)){
+			audioSource.Stop();
 		}
 
 		if (Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.forward);
 			print("Roate left");
 		} else if (Input.GetKey(KeyCode.D)){
+			transform.Rotate(Vector3.back);
 			print("Roate left");
 		}
 	}
