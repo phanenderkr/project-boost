94655c45d16256a2c50899e45431bee14d336917
Phanender Reddy Kasireddy <phanender100@gmail.com>
2017-11-28 19:19:33 +0530

    Basic level

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Rocket : MonoBehaviour{
 	private Rigidbody rigidBody;
 	private AudioSource audioSource;
-	
+	[SerializeField]
+	float rcsThrust = 100f, thrust =1000f;
 	// Use this for initialization
 	void Start (){
 		rigidBody = gameObject.GetComponent<Rigidbody>();
@@ -14,27 +15,44 @@ public class Rocket : MonoBehaviour{
 	
 	// Update is called once per frame
 	void Update (){
-		ProcessInput();
+		Thrust();
+		Rotate();
 	}
 
-	void ProcessInput(){
-		//can thrust while rotating
+	void Rotate(){
+		rigidBody.freezeRotation = true;
+		
+		float rotationSpeed = rcsThrust * Time.deltaTime;
+		if (Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.forward * rotationSpeed);
+		} else if (Input.GetKey(KeyCode.D)){
+			transform.Rotate(Vector3.back * rotationSpeed);
+		}
+		rigidBody.freezeRotation = false;
+	}
+
+	private void Thrust(){
 		if (Input.GetKey(KeyCode.Space)){
-			rigidBody.AddRelativeForce(Vector3.up * 1);
+			float thrustSpeed = thrust * Time.deltaTime;
+//			print(thrust + ", " + Time.deltaTime);
+			rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
 			if (!audioSource.isPlaying){
 				audioSource.Play();
-			}	
+			}
 		}
 		if (Input.GetKeyUp(KeyCode.Space)){
 			audioSource.Stop();
 		}
+	}
 
-		if (Input.GetKey(KeyCode.A)){
-			transform.Rotate(Vector3.forward);
-			print("Roate left");
-		} else if (Input.GetKey(KeyCode.D)){
-			transform.Rotate(Vector3.back);
-			print("Roate left");
+	private void OnCollisionEnter(Collision other){
+		switch (other.gameObject.tag){
+			case "Friendly":
+				print("Ok");
+				break;
+			default:
+				print("Dead");
+				break;
 		}
 	}
 }
