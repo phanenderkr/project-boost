b162325e03027bed9b284af87ecea628c38acf98
Phanender Reddy Kasireddy <phanender100@gmail.com>
2017-12-01 14:32:18 +0530

    Multiple Audio Files

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour{
+	private Vector3 change;
+
+	private bool up = true;
+	// Use this for initialization
+	void Start () {
+		change = gameObject.transform.position;
+		print(change);
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if (gameObject.transform.position.y >= 28){
+			up = false;
+		}else if (gameObject.transform.position.y <= 10.8){
+			up = true;
+		}
+		if (up){
+			change.y += 0.1f;
+		} else{
+			change.y -= 0.1f;
+		}
+		
+		gameObject.transform.position = change;
+		print(change);
+	}
+}
--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour{
 	private Rigidbody rigidBody;
 	private AudioSource audioSource;
+	
 	[SerializeField]
-	float rcsThrust = 100f, thrust =1000f;
+	float rcsThrust = 100f, mainThrust =1000f;
+
+	[SerializeField] private AudioClip mainEngine, deathClip, successClip; 
+
+	enum State{
+		Alive,
+		Dying, 
+		Transcending
+	}
+
+	private State state = State.Alive;
+	
 	// Use this for initialization
 	void Start (){
 		rigidBody = gameObject.GetComponent<Rigidbody>();
@@ -15,44 +26,87 @@ public class Rocket : MonoBehaviour{
 	
 	// Update is called once per frame
 	void Update (){
-		Thrust();
-		Rotate();
+		//todo somewhere stop the sound
+		if (state == State.Alive){
+			RespondToThrustInput();
+			RespondToRotateInput();
+		}
 	}
 
-	void Rotate(){
+	//---------------------------------------------------------------------------------
+	//--------------------------------Motion------------------------------------------
+	//---------------------------------------------------------------------------------
+	void RespondToRotateInput(){
 		rigidBody.freezeRotation = true;
-		
 		float rotationSpeed = rcsThrust * Time.deltaTime;
-		if (Input.GetKey(KeyCode.A)){
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
 			transform.Rotate(Vector3.forward * rotationSpeed);
-		} else if (Input.GetKey(KeyCode.D)){
+		} else if (Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)){
 			transform.Rotate(Vector3.back * rotationSpeed);
 		}
 		rigidBody.freezeRotation = false;
 	}
 
-	private void Thrust(){
-		if (Input.GetKey(KeyCode.Space)){
-			float thrustSpeed = thrust * Time.deltaTime;
-//			print(thrust + ", " + Time.deltaTime);
-			rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
-			if (!audioSource.isPlaying){
-				audioSource.Play();
-			}
+	private void RespondToThrustInput(){
+		if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) ){
+			ApplyThrust();
 		}
-		if (Input.GetKeyUp(KeyCode.Space)){
+		if (Input.GetKeyUp(KeyCode.Space)|| Input.GetKeyUp(KeyCode.UpArrow)){
 			audioSource.Stop();
 		}
 	}
 
+	private void ApplyThrust(){
+		float thrustSpeed = mainThrust * Time.deltaTime;
+		rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
+		if (!audioSource.isPlaying){
+			audioSource.PlayOneShot(mainEngine);
+		}
+	}
+
+	//---------------------------------------------------------------------------------
+	//--------------------------------Collision----------------------------------------
+	//---------------------------------------------------------------------------------
+	
 	private void OnCollisionEnter(Collision other){
+		
+		// prevents the execution the future statements
+		if (state != State.Alive){
+			return;
+		}
+		
 		switch (other.gameObject.tag){
 			case "Friendly":
 				print("Ok");
 				break;
+			case "Finish":
+				StartFinishSquence();
+				break;
 			default:
-				print("Dead");
+				StartDeathSequence();
 				break;
 		}
+	}
+
+	private void StartFinishSquence(){
+		audioSource.Stop();
+		audioSource.PlayOneShot(successClip);
+		state = State.Transcending;
+		Invoke("LoadNextLevel",1f);
+	}
+
+	private void StartDeathSequence(){
+		audioSource.Stop();
+		audioSource.PlayOneShot(deathClip);
+		state = State.Dying;
+		Invoke("LoadFirstLevel",1f);
+	}
+	
+	private void LoadFirstLevel(){
+		SceneManager.LoadScene(0);
+	}
+
+	private void LoadNextLevel(){
+		SceneManager.LoadScene(1); // todo allow more than 2 levels
 	}
 }
